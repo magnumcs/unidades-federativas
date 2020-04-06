5aabe3fe5d933a6bea5a7a7e0799a824312e9998
magnumcs <magnumcs@outlook.com>
2020-04-06 18:50:50 -0300

    Ajustes config heroku api

--- unidade-federativa-app/src/app/common/config.js
+++ unidade-federativa-app/src/app/common/config.js
@@ -1,10 +1,10 @@
 export default {
     appName: 'UnidadeFederativa',
     api: {
-        // base: 'https://unidade-federativa-api.herokuapp.com',
-        base: 'http://localhost:8080',
+        base: 'https://unidades-federativas-api.herokuapp.com',
+        // base: 'http://localhost:8080',
         resources: {
-            unidadefederativa: '/api/unidade-federativa'
+            unidadefederativa: '/api/unidades-federativas'
         }
     },
     stateUrls: {

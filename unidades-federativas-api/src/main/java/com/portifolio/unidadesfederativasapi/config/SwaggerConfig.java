1e5a27228fe234097fb02233fdebec3a908c114c
magnumcs <magnumcs@outlook.com>
2020-04-06 00:48:58 -0300

    ajuste swagger config

--- unidades-federativas-api/src/main/java/com/portifolio/unidadesfederativasapi/config/SwaggerConfig.java
+++ unidades-federativas-api/src/main/java/com/portifolio/unidadesfederativasapi/config/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.portifoliio.unidadesfederativas"))
+                .apis(RequestHandlerSelectors.basePackage("com.portifolio.unidadesfederativas"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
